ffa8e566d0e090eac8b7b9739524722cf6b5d8cf
unknown <krajubraju@gmail.com>
2019-04-30 03:02:13 -0700

    Create new SPA Folder and Install Routing DOM

---
+++ reactjs_spa/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+
+{/* <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      
+      <img src={logo} className="App-logo" height ="75" width = "75" alt="logo" />
+      
+    </div>
+    <ul className="nav navbar-nav">
+      <li className="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav> */}
+<br/>
+
+<nav className="navbar navbar-inverse">
+  <div className="container-fluid">
+    <div className="navbar-header">
+    <img src={logo} className="App-logo" height ="75" width = "75" alt="logo" />
+    </div>
+    <ul className="nav navbar-nav">
+      <li className="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul className="nav navbar-nav navbar-right">
+      <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+    </div>
+  );
+}
+
+export default App;
