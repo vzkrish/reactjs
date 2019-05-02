cadcb5fb79a5e73a88bdca291489b175224b519c
unknown <krajubraju@gmail.com>
2019-05-02 04:00:05 -0700

    Staged Store Data

---
+++ reduxapp/src/store/store.jsx
@@ -0,0 +1,26 @@
+export default  [
+    {
+        id: 1,
+        first: "Krishnam Raju",
+        last: "Bhupathiraju",
+        age: 37,
+        description: ".Net 4.5, MVC 6, React with Redux, Angular 2,Node js",
+        thumbnail: "http://imgur.com/KU3b5Wn.jpg"
+    },
+    {
+        id: 2,
+        first: "Anvi",
+        last: "Sri",
+        age: 05,
+        description: "C,C++,Java,J2EE,Spring Rest Services,Hibernate",
+        thumbnail: "http://imgur.com/5fn3jRe.jpg"
+    },
+    {
+        id: 3,
+        first: "Ashrith",
+        last: "Varma",
+        age: 16,
+        description: "Softskills, Communication skills, Managerial skills",
+        thumbnail: "http://imgur.com/Obf5SniV.jpg"
+    }
+]
