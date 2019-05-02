dec50e0e38b3825e6e26f1dde7eb9ca7738402d8
unknown <krajubraju@gmail.com>
2019-05-02 04:15:38 -0700

    Create an Index for Reducers to combine all the reduces and share one pointer to access them

---
+++ reduxapp/src/reducers/index.js
@@ -0,0 +1,11 @@
+//This Index is used to combine and expose to the react guys
+import {combineReducers} from 'redux'
+import TrainerReducer from './reducer-trainers'
+import ActiveTrainerReducer from "./reducer-active-trainer"
+
+const allReaders = combineReducers({
+    trainers: TrainerReducer,
+    activeTrainer: ActiveTrainerReducer
+});
+
+export default allReducers
